8c1b9d3609345459541b77d468a11e15658195ec
alejandromotta <alejandromotta808@gmail.com>
2024-01-17 18:13:42 -0500

    testing

--- src/pages/api/certified/index.js
+++ src/pages/api/certified/index.js
@@ -1,18 +1,18 @@
-const { PrismaClient } = require("@prisma/client")
+//const { PrismaClient } = require("@prisma/client")
 
-const prisma = new PrismaClient()
+//const prisma = new PrismaClient()
 
 const handler = async (req, res) =>{
   console.log(req.method)
   if(req.method === 'GET'){
-    const certifieds = await  prisma.certified.findMany();
- return res?.status(200).json(certifieds)
+    //const certifieds = await  prisma.certified.findMany();
+ return res?.status(200).json(["certifieds"])
   }
   if(req.method === 'POST'){
-    const newData = await prisma.certified.create({
-      data: req.body
-    })
-   return res?.status(200).json(newData)
+    // const newData = await prisma.certified.create({
+    //   data: req.body
+    // })
+   return res?.status(200).json([])
   }
   return res?.status(200).json({ props: "soy otro"})
 }
--- src/pages/api/client/index.js
+++ src/pages/api/client/index.js
@@ -1,18 +1,18 @@
-const { PrismaClient } = require("@prisma/client")
+// const { PrismaClient } = require("@prisma/client")
 
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
 const handler = async (req, res) =>{
   console.log(req.method)
   if(req.method === 'GET'){
-    const clients = await  prisma.client.findMany();
- return res?.status(200).json(clients)
+    // const clients = await  prisma.client.findMany();
+ return res?.status(200).json(["alooo"])
   }
   if(req.method === 'POST'){
-    const newData = await prisma.client.create({
-      data: req.body
-    })
-   return res?.status(200).json(newData)
+    // const newData = await prisma.client.create({
+    //   data: req.body
+    // })
+   return res?.status(200).json([])
   }
   return res?.status(200).json({ props: "soy otro"})
 }
