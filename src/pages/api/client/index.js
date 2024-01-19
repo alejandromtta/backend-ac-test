68692b362f025b3e9e01c6beaf5e404861e5c958
alejandromotta <alejandromotta808@gmail.com>
2024-01-18 20:58:05 -0500

    added all

--- src/pages/api/client/index.js
+++ src/pages/api/client/index.js
@@ -1,17 +1,16 @@
-const {prisma} = require('../../../const/db')
-const handler = async (req, res) =>{
- 
-  if(req.method === 'GET'){
-    const clients = await  prisma.client.findMany();
- return res?.status(200).json(clients)
+const { prisma } = require("../../../const/db");
+const handler = async (req, res) => {
+  if (req.method === "GET") {
+    const clients = await prisma.client.findMany({include: { certified: true}});
+    return res?.status(200).json(clients);
   }
-  if(req.method === 'POST'){
+  if (req.method === "POST") {
     const newData = await prisma.client.create({
-      data: req.body
-    })
-   return res?.status(200).json(newData)
+      data: req.body,
+    });
+    return res?.status(200).json(newData);
   }
-  return res?.status(200).json({ props: "soy otro"})
-}
+  return res?.status(200).json({ props: "soy otro" });
+};
 
 export default handler;
