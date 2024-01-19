4b6e59d5783c5ff5bf39f527ad747b4d5ab460a4
alejandromotta <alejandromotta808@gmail.com>
2024-01-18 20:16:30 -0500

    fix

--- src/pages/api/client/index.js
+++ src/pages/api/client/index.js
@@ -1,15 +1,15 @@
-const prisma = require('../../../const/db')
+const {get} = require('../../../const/db')
 const handler = async (req, res) =>{
-  console.log(prisma)
+  console.log(get())
   if(req.method === 'GET'){
     //  const clients = await  prisma.client.findMany();
  return res?.status(200).json('clients')
   }
   if(req.method === 'POST'){
-    const newData = await prisma.client.create({
-      data: req.body
-    })
-   return res?.status(200).json(newData)
+    // const newData = await prisma.client.create({
+    //   data: req.body
+    // })
+   return res?.status(200).json('as')
   }
   return res?.status(200).json({ props: "soy otro"})
 }
